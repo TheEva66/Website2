956cbb29999ee80d1f80002e33106c4acae14d4a
-insert name here- <104537575+TheEva66@users.noreply.github.com>
2024-09-23 12:42:27 -0400

    initial change

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -44,8 +44,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: 'Evas Portfolio',
+  description: "Eva's portfolio website",
 };
 
 /**
@@ -108,12 +108,11 @@ export const aboutData: About = {
   to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
   in.`,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: BuildingOffice2Icon},
+    {label: 'Location', text: 'Richboro PA', Icon: MapIcon},
+    {label: 'Age', text: '18', Icon: CalendarIcon},
+    {label: 'Interests', text: 'Gaming, VR, Coding', Icon: SparklesIcon},
+    {label: 'Study', text: 'Arcadia University', Icon: AcademicCapIcon},
+    {label: 'Employment', text: 'Pulse Entertainment', Icon: BuildingOffice2Icon},
   ],
 };
 
@@ -127,65 +126,46 @@ export const skills: SkillGroup[] = [
       {
         name: 'English',
         level: 10,
-      },
-      {
-        name: 'French',
-        level: 4,
-      },
-      {
-        name: 'Spanish',
-        level: 3,
-      },
+      }
     ],
   },
   {
     name: 'Frontend development',
     skills: [
       {
-        name: 'React',
-        level: 9,
+        name: 'HTML/CSS',
+        level: 4,
       },
       {
-        name: 'Typescript',
-        level: 7,
-      },
-      {
-        name: 'GraphQL',
-        level: 6,
-      },
+        name: 'JS/TS',
+        level: 1,
+      }
     ],
   },
   {
     name: 'Backend development',
     skills: [
       {
-        name: 'Node.js',
+        name: 'Python',
         level: 8,
       },
       {
-        name: 'Rust',
-        level: 5,
+        name: 'Java',
+        level: 7,
       },
-      {
-        name: 'Golang',
-        level: 4,
-      },
+
     ],
   },
   {
-    name: 'Mobile development',
+    name: 'Unreal Engine',
     skills: [
       {
-        name: 'React Native',
-        level: 9,
+        name: 'Blueprints',
+        level: 7,
       },
       {
-        name: 'Flutter',
-        level: 4,
-      },
-      {
-        name: 'Swift',
-        level: 3,
+        name: 'Map design',
+        level: 5,
       },
     ],
   },
@@ -268,39 +248,39 @@ export const portfolioItems: PortfolioItem[] = [
  */
 export const education: TimelineItem[] = [
   {
-    date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: 'June 2024',
+    location: 'Council Rock High School South',
+    title: 'High School Diploma',
   },
   {
-    date: 'March 2003',
-    location: 'School of Business',
-    title: 'What did you study 101',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: 'September 2024 - June 2028',
+    location: 'Arcadia University',
+    title: 'Bachelor of Science in Computer Science',
+    content: <p>Expected graduation date is 2028</p>,
   },
 ];
 
 export const experience: TimelineItem[] = [
   {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
+    date: 'June 2019 - Present',
+    location: 'Pulse Entertainment',
+    title: 'Lead Lighting Designer',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        I worked for Pulse Entertainment to design and implement lighting for various events. I was responsible for light 
+        fixture locations aswell as the programming of the lights. I also worked with the sound team to ensure that the lighting and sound were in sync.
       </p>
     ),
   },
   {
-    date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
-    title: 'Junior bug fixer',
+    date: '2021 - Present',
+    location: 'Newtown Arts Company',
+    title: 'Tech',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        I worked for Newtown Arts Company on many productions and was responsable for 
+        the lighting, sound, spotlight, projections and many other technical aspects of the show.
+        I was the recipiant of the TODO ADD NAME legacy scholarship.
       </p>
     ),
   },
@@ -340,23 +320,18 @@ export const contact: ContactSection = {
   items: [
     {
       type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
+      text: 'evaakselrad@gmail.com',
+      href: 'mailto:evaakselrad@gmail.com',
     },
     {
-      type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
-    },
-    {
-      type: ContactType.Instagram,
-      text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
+      type: ContactType.LinkedIn,
+      text: 'Tim Baker',
+      href: 'https://www.linkedin.com/in/timbakerx/',
     },
     {
       type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
+      text: 'TheEva66',
+      href: 'https://github.com/TheEva66',
     },
   ],
 };
@@ -365,9 +340,7 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx'},
+  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/TheEva66'},
   {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
   {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/timbakerx/'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/reactresume/'},
-  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
 ];

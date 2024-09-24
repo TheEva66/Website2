409f3260005d589a2949d6000e4df08df8f01cc6
-insert name here- <104537575+TheEva66@users.noreply.github.com>
2024-09-24 14:52:18 -0400

    E

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -1,8 +1,7 @@
-import {BoltIcon, ChevronUpIcon} from '@heroicons/react/24/solid';
+import {ChevronUpIcon} from '@heroicons/react/24/solid';
 import {FC, memo} from 'react';
 
 import {SectionId} from '../../data/data';
-import Socials from '../Socials';
 
 
 const Footer: FC = memo(() => (

22ee9da30e1d1a510119ac4fa496ca7de992d50f
-insert name here- <104537575+TheEva66@users.noreply.github.com>
2024-09-24 14:51:56 -0400

    fix

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -14,18 +14,7 @@ const Footer: FC = memo(() => (
         <ChevronUpIcon className="h-6 w-6 bg-transparent sm:h-8 sm:w-8" />
       </a>
     </div>
-    <div className="flex flex-col items-center gap-y-6">
-      <div className="flex gap-x-4 text-neutral-500">
-        <Socials />
-      </div>
-      <a
-        className="-m-2 flex items-center gap-x-1 rounded-md p-2 ring-yellow focus:outline-none focus:ring-2"
-        href="https://reactresume.com">
-        <BoltIcon className="h-5 w-5 text-yellow" />
 
-        
-      </a>
-    </div>
   </div>
 ));
 

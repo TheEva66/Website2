fdeadff393172cafdc2ed6e0889ba3e244649324
-insert name here- <104537575+TheEva66@users.noreply.github.com>
2024-09-25 22:01:20 -0400

    dwa

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -66,8 +66,7 @@ export type SectionId = (typeof SectionId)[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Eva Akselrad 
-  To josh's friends go away pls`,
+  name: `I'm Eva Akselrad`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">

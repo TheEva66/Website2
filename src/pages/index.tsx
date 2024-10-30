0fb844b1560b59bd3dc652007f14b7201f0f6e9d
-insert name here- <104537575+TheEva66@users.noreply.github.com>
2024-10-30 09:50:01 -0400

    AHHHHHHH

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import { useRouter } from 'next/router';
 import dynamic from 'next/dynamic';
-import { FC, memo } from 'react';
+import {useRouter} from 'next/router';
+import {FC, memo} from 'react';
 
 import About from '../components/Sections/About';
 import Contact from '../components/Sections/Contact';
@@ -11,12 +11,12 @@ import Page from '../components/Layout/Page';
 import Portfolio from '../components/Sections/Portfolio';
 import Resume from '../components/Sections/Resume';
 import Testimonials from '../components/Sections/Testimonials';
-import { homePageMeta } from '../data/data';
+import {homePageMeta} from '../data/data';
 
 const Header = dynamic(() => import('../components/Sections/Header'), {ssr: false});
 
 const Home: FC = memo(() => {
-  const { title, description } = homePageMeta;
+  const {title, description} = homePageMeta;
   const router = useRouter();
 
   return (

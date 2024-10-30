e8bda04fb97dabf65b7439813a57feea73a6a53e
-insert name here- <104537575+TheEva66@users.noreply.github.com>
2024-10-30 09:59:44 -0400

    fy103

---
+++ src/pages/fy103.tsx
@@ -0,0 +1,9 @@
+// /pages/fy103.tsx
+import { FC } from 'react';
+import GobletOfFire from '../components/Sections/GobletOfFire';
+
+const FY103: FC = () => {
+  return <GobletOfFire />;
+};
+
+export default FY103;

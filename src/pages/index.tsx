fc1b2da9ed0d1daffe187ce7b6a85e881a8dbe38
-insert name here- <104537575+TheEva66@users.noreply.github.com>
2024-10-30 09:43:42 -0400

    AHHHHHH

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,19 @@
+import { useRouter } from 'next/router';
 import dynamic from 'next/dynamic';
 import { FC, memo } from 'react';
 
+import Page from '../components/Layout/Page';
 import About from '../components/Sections/About';
 import Contact from '../components/Sections/Contact';
 import Footer from '../components/Sections/Footer';
 import Hero from '../components/Sections/Hero';
-import Page from '../components/Layout/Page';
 import Portfolio from '../components/Sections/Portfolio';
 import Resume from '../components/Sections/Resume';
 import Testimonials from '../components/Sections/Testimonials';
 import { homePageMeta } from '../data/data';
+import GobletOfFire from '../components/Sections/GobletOfFire';
 
-
-// eslint-disable-next-line react-memo/require-memo
-const Header = dynamic(() => import('../components/Sections/Header'), {ssr: false});
-
-
+const Header = dynamic(() => import('../components/Sections/Header'), { ssr: false });
 
 const Home: FC = memo(() => {
   const { title, description } = homePageMeta;
@@ -35,6 +33,5 @@ const Home: FC = memo(() => {
     </Page>
   );
 });
-
 
 export default Home;
